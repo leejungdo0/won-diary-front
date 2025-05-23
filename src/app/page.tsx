f23d17d639df9e8df7cba5482c85bb6b484c492a
lee.jungdo@gmail.com <lee.jungdo@gmail.com>
2025-05-23 20:42:04 +0900

    add work in progress text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-4 space-y-8">
       <h1 className="text-2xl font-semibold">상시일기</h1>
+      <div className="block italic">- work in progress -</div>
       <MirijoonbiWrapper />
       <TimeInput />
       <Gyemun />
