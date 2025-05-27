e8b1a29a6e422098023a4f0acb481137d185a183
lee.jungdo@gmail.com <lee.jungdo@gmail.com>
2025-05-27 16:04:52 +0900

    reset localStorage if today is a new day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+"use client"
+
 import Gyemun from "@/components/Gyemun";
 import MirijoonbiWrapper from "@/components/MirijoonbiWrapper";
 import ResetLocalStorageButton from "@/components/ResetLocalStorageButton";
 import TimeInput from "@/components/TimeInput";
+import { useEffect } from "react";
+
+
 
 export default function Home() {
+  const getTodayDateString = () => new Date().toISOString().split("T")[0];
+  
+  // reset localStorage if today is a new day  
+  useEffect(() => {
+        if (typeof window === "undefined") return;
+        const lastClearDate = localStorage.getItem("lastClearDate");
+        const today = getTodayDateString();
+        if (lastClearDate !== today) {
+          localStorage.clear();
+          localStorage.setItem("lastClearDate", today);
+        }
+      }, []);
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-center p-4 space-y-8">
       <h1 className="text-2xl font-semibold">상시일기</h1>
