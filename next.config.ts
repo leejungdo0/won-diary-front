7fa44e661eb7c83f5b6b461760e43d2d77e4e9c3
lee.jungdo@gmail.com <lee.jungdo@gmail.com>
2025-05-29 17:56:02 +0900

    add next config options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // React 엄격 모드 활성화
+  swcMinify: true, // SWF 파일 최소화
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 중 ESLint 오류 무시
+  },
 };
 
 export default nextConfig;
