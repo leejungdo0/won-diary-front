6f95fd723f4d74e07547fcf05c5db806ee2a4812
lee.jungdo@gmail.com <lee.jungdo@gmail.com>
2025-05-29 18:38:06 +0900

    use Analytics component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import ResetLocalStorageButton from "@/components/ResetLocalStorageButton";
 import TimeInput from "@/components/TimeInput";
 import { getTodayDateString } from "@/lib/utils";
 import { useEffect } from "react";
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
@@ -22,13 +21,16 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center p-4 space-y-8">
-      <h1 className="text-2xl font-semibold">상시일기</h1>
-      <div className="block italic">- work in progress -</div>
-      <MirijoonbiWrapper />
-      <TimeInput />
-      <GyeMoon />
-      <ResetLocalStorageButton />
-    </main>
+    <>
+      <Analytics />
+      <main className="min-h-screen flex flex-col items-center justify-center p-4 space-y-8">
+        <h1 className="text-2xl font-semibold">상시일기</h1>
+        <div className="block italic">- work in progress -</div>
+        <MirijoonbiWrapper />
+        <TimeInput />
+        <GyeMoon />
+        <ResetLocalStorageButton />
+      </main>
+    </>
   );
 }
